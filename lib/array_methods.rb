2fa328bfd05a0d6d22df63ba4d59f35d6b498ae4
Nicole Lucien <nicolemlucien@gmail.com>
2019-09-27 02:37:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   array = [1, 2, 3, 4, 5]
-  {array.include?(5)
-  puts array.index}
+  array.include?(5)
+  puts array.index
 end
 
 def find_max_value(array)
